09e694daf6d19a7757009744f48e054e851f1a48
Marco Macherelli <elmache05@gmail.com>
2024-10-19 12:51:53 +0200

    Reset Password Fix

--- Routes/Authentication/authenticationUPDATE.js
+++ Routes/Authentication/authenticationUPDATE.js
@@ -2,11 +2,10 @@
 const express = require("express");
 const router = express.Router();
 const AuthenticationController = require("../../Controllers/AuthenticationController");
-const authenticateMiddleware = require("../../middlewares/EmailService/Authentication/Authmiddleware");
 
 const authenticationUPDATE = (db) => {
   // Definisci le route Update qui
-  router.put("/ResetPassword", authenticateMiddleware, async (req, res) => {
+  router.put("/ResetPassword", async (req, res) => {
     AuthenticationController.ResetPassword(req, res, db);
   });
 
