2829a6956d3c90ab10879f28446f148240a95eba
Andrea Braia <andrix.braia@gmail.com>
2024-11-29 20:18:51 +0100

    Fix websocket

--- socket.js
+++ socket.js
@@ -6,7 +6,11 @@ let onlineUsers = [];
 const createSocketServer = (httpServer) => {
   const io = new Server(httpServer, {
     cors: {
-      origin: ["http://localhost:5173", "http://localhost:5174"],
+      origin: [
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "https://app.spacedesign-italia.it",
+      ],
       methods: ["GET", "POST"],
       credentials: true,
     },
